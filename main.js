d2533f2b40d6e24d3236ab7ba35f52e8c60c5626
f-m-a-ly <farah.ms.aly@gmail.com>
2022-06-06 13:55:40 +0200

    Done

--- main.js
+++ main.js
@@ -23,6 +23,9 @@ const selfCarePrevious = document.getElementById('selfCarePrevious')
 const activityTime = [workTime, playTime, studyTime, exerciseTime, socialTime, selfCareTime]
 const activityPrevious = [workPrevious, playPrevious, studyPrevious, exercisePrevious, socialPrevious, selfCarePrevious]
 
+let currentHours = ''
+let previousHours = ''
+
 GetDaily()
 
 
@@ -35,23 +38,16 @@ function GetDaily() {
     return response.json();
   })
   .then(jsondata => {
-    // workTime.innerText = jsondata[0].timeframes.daily.current + 'hrs'
-    // workPrevious.innerText = 'Previous - ' + jsondata[0].timeframes.daily.previous + 'hrs'
+    for (let i = 0; i < activityTime.length; i++) {
+      if (jsondata[i].timeframes.daily.current > 1 || jsondata[i].timeframes.daily.current == 0) currentHours = 'hrs'
+      else currentHours = 'hr'
 
-    // playTime.innerText = jsondata[1].timeframes.daily.current + 'hr'
-    // playPrevious.innerText = 'Previous - ' + jsondata[1].timeframes.daily.previous + 'hrs'
+      if (jsondata[i].timeframes.daily.previous > 1 || jsondata[i].timeframes.daily.previous == 0) previousHours = 'hrs'
+      else previousHours = 'hr'
 
-    // studyTime.innerText = jsondata[2].timeframes.daily.current + 'hrs'
-    // studyPrevious.innerText = 'Previous - ' + jsondata[2].timeframes.daily.previous + 'hrs'
-
-    // exerciseTime.innerText = jsondata[3].timeframes.daily.current + 'hr'
-    // exercisePrevious.innerText = 'Previous - ' + jsondata[3].timeframes.daily.previous + 'hr'
-
-    // socialTime.innerText = jsondata[4].timeframes.daily.current + 'hr'
-    // socialPrevious.innerText = 'Previous - ' + jsondata[4].timeframes.daily.previous + 'hrs'
-
-    // selfCareTime.innerText = jsondata[5].timeframes.daily.current + 'hr'
-    // selfCarePrevious.innerText = 'Previous - ' + jsondata[5].timeframes.daily.previous + 'hr'
+      activityTime[i].innerText = jsondata[i].timeframes.daily.current + currentHours;
+      activityPrevious[i].innerText = 'Previous - ' +  jsondata[i].timeframes.daily.previous + previousHours;
+    }
  })
 }
 
@@ -64,23 +60,16 @@ function GetWeekly() {
     return response.json();
   })
   .then(jsondata => {
-    workTime.innerText = jsondata[0].timeframes.weekly.current + 'hrs'
-    workPrevious.innerText = 'Last Week - ' + jsondata[0].timeframes.weekly.previous + 'hrs'
+    for (let i = 0; i < activityTime.length; i++) {
+      if (jsondata[i].timeframes.weekly.current > 1 || jsondata[i].timeframes.weekly.current == 0) currentHours = 'hrs'
+      else currentHours = 'hr'
 
-    playTime.innerText = jsondata[1].timeframes.weekly.current + 'hrs'
-    playPrevious.innerText = 'Last Week - ' + jsondata[1].timeframes.weekly.previous + 'hrs'
+      if (jsondata[i].timeframes.weekly.previous > 1 || jsondata[i].timeframes.weekly.previous == 0) previousHours = 'hrs'
+      else previousHours = 'hr'
 
-    studyTime.innerText = jsondata[2].timeframes.weekly.current + 'hrs'
-    studyPrevious.innerText = 'Last Week - ' + jsondata[2].timeframes.weekly.previous + 'hrs'
-
-    exerciseTime.innerText = jsondata[3].timeframes.weekly.current + 'hrs'
-    exercisePrevious.innerText = 'Last Week - ' + jsondata[3].timeframes.weekly.previous + 'hrs'
-
-    socialTime.innerText = jsondata[4].timeframes.weekly.current + 'hrs'
-    socialPrevious.innerText = 'Last Week - ' + jsondata[4].timeframes.weekly.previous + 'hrs'
-
-    selfCareTime.innerText = jsondata[5].timeframes.weekly.current + 'hrs'
-    selfCarePrevious.innerText = 'Last Week - ' + jsondata[5].timeframes.weekly.previous + 'hrs'
+      activityTime[i].innerText = jsondata[i].timeframes.weekly.current + currentHours;
+      activityPrevious[i].innerText = 'Last Week - ' +  jsondata[i].timeframes.weekly.previous + previousHours;
+    }
  })
 }
 
@@ -93,23 +82,16 @@ function GetMonthly() {
     return response.json();
   })
   .then(jsondata => {
-    workTime.innerText = jsondata[0].timeframes.monthly.current + 'hrs'
-    workPrevious.innerText = 'Last Month - ' + jsondata[0].timeframes.monthly.previous + 'hrs'
+    for (let i = 0; i < activityTime.length; i++) {
+      if (jsondata[i].timeframes.monthly.current > 1 || jsondata[i].timeframes.monthly.current == 0) currentHours = 'hrs'
+      else currentHours = 'hr'
 
-    playTime.innerText = jsondata[1].timeframes.monthly.current + 'hrs'
-    playPrevious.innerText = 'Last Month - ' + jsondata[1].timeframes.monthly.previous + 'hrs'
+      if (jsondata[i].timeframes.monthly.previous > 1 || jsondata[i].timeframes.monthly.previous == 0) previousHours = 'hrs'
+      else previousHours = 'hr'
 
-    studyTime.innerText = jsondata[2].timeframes.monthly.current + 'hrs'
-    studyPrevious.innerText = 'Last Month - ' + jsondata[2].timeframes.monthly.previous + 'hrs'
-
-    exerciseTime.innerText = jsondata[3].timeframes.monthly.current + 'hrs'
-    exercisePrevious.innerText = 'Last Month - ' + jsondata[3].timeframes.monthly.previous + 'hrs'
-
-    socialTime.innerText = jsondata[4].timeframes.monthly.current + 'hrs'
-    socialPrevious.innerText = 'Last Month - ' + jsondata[4].timeframes.monthly.previous + 'hrs'
-
-    selfCareTime.innerText = jsondata[5].timeframes.monthly.current + 'hrs'
-    selfCarePrevious.innerText = 'Last Month - ' + jsondata[5].timeframes.monthly.previous + 'hrs'
+      activityTime[i].innerText = jsondata[i].timeframes.monthly.current + currentHours;
+      activityPrevious[i].innerText = 'Last Month - ' +  jsondata[i].timeframes.monthly.previous + previousHours;
+    }
  })
 }
 
